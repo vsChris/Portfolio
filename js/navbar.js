e80358bf108bceff9e832a449bbf767c90757854
vsChris <>
2021-02-02 16:17:37 -0500

    Continuing development of HTML and CSS

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+// const hamClosed = document.querySelector('.hamburger');
+// const hamOpen = document.querySelector('.hamburger-open');
+
+// console.log(hamClosed);
+// console.log(hamOpen);
+
+// hamClosed.addEventListener('click', function (e) {
+//   console.log(e);
+// });
