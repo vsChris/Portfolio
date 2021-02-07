01dec313cba621e30ed8c60c548814fc4664e513
vsChris <>
2021-02-06 22:14:39 -0500

    Continuing development of HTML and CSS

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,30 @@
-// const hamClosed = document.querySelector('.hamburger');
-// const hamOpen = document.querySelector('.hamburger-open');
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-list-link');
+// console.log(navLinks);
+let menuOpen = false;
 
-// console.log(hamClosed);
-// console.log(hamOpen);
+function isMenuOpen() {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+    nav.classList.add('show');
+    // navLinks.forEach((link) => link.classList.add('display'));
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    nav.classList.remove('show');
+    // navLinks.forEach((link) => link.classList.remove('display'));
+  }
+}
 
-// hamClosed.addEventListener('click', function (e) {
-//   console.log(e);
-// });
+function closeEvent(e) {
+  if (menuOpen) {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    nav.classList.remove('show');
+  }
+}
+
+menuBtn.addEventListener('click', isMenuOpen);
+navLinks.forEach((navLink) => navLink.addEventListener('click', closeEvent));
